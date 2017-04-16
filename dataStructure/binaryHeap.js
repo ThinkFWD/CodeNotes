32b90040766d8184017154a0b6fc244cdd61e231
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-16 18:33:31 -0400

    added binary Min Stack

---
+++ dataStructure/binaryHeap.js
@@ -0,0 +1,17 @@
+// +++++++++++++++++++++++++++++++++++++++
+// ++Implementation of Binary Heap (Min)++
+// +++++++++++++++++++++++++++++++++++++++
+
+const binaryMinHeap = () => {
+	this.list = [];
+};
+
+binaryMinHeap.prototype.push(node){
+// push element to bottom
+// allow it to bubble up
+}
+
+binaryMinHeap.prototype.pop(){
+// store first element so we can return it
+// get element at the end of array
+}
