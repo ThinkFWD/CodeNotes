2dfb28c6ecd2eb922b974ae1f67f5004b616e12a
ThinkFWD <mike.thinkfwd@gmail.com>
2017-05-02 09:07:05 -0400

    finished hash table

--- dataStructure/hashTable.js
+++ dataStructure/hashTable.js
@@ -5,6 +5,7 @@
 
 const hashTable = function (){
   this.table = new Array(137);
+  this.createChain();
 }
 
 hashTable.prototype.simpleHash = function (value){
@@ -15,30 +16,42 @@ hashTable.prototype.simpleHash = function (value){
   return total % this.table.length;
 }
 
-hashTable.prototype.insert = function (data){
-  let position = this.simpleHash(data);
-  this.table[position] = data;
+hashTable.prototype.insert = function (key, data){
+  let position = this.simpleHash(key);
+  this.table[position].push(data);
+}
+
+hashTable.prototype.retrieve = function (key){
+  return this.table[this.simpleHash(key)];
 }
 
 hashTable.prototype.showDist = function () {
   for (let x = 0; x < this.table.length; x++){
-  	if(this.table[x] != undefined){
+  	if(this.table[x][0] != undefined){
   	  console.log(x + " - " + this.table[x]);
   	}
   }
 }
 
+hashTable.prototype.createChain = function () {
+  for (let x = 0; x < this.table.length; x++){
+    this.table[x] = new Array();
+  }
+}
+
+
 
 // EXAMPLE TEST CASE
 
 let profiles = new hashTable;
-profiles.insert('Michael');
-profiles.insert('John');
-profiles.insert('Fred');
-profiles.insert('Max');
+profiles.insert('hello', 'Michael');
+profiles.insert('johnny', 'John');
+profiles.insert('bingo', 'Fred');
+profiles.insert('bingo', 'Fred2');
+profiles.insert('test', 'Max');
 console.log('___________ HASH TABLES _____________')
 profiles.showDist();
-
-
+console.log('Seach for hello', profiles.retrieve('hello'))
+console.log(profiles)
 
 
