63634643beeab3c276096adb863f023d64c6bea0
ThinkFWD <mike.thinkfwd@gmail.com>
2017-05-03 09:50:55 -0400

    completed bubbleSort

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -16,6 +16,23 @@ class testArray extends Array {
   randomNum(max=100,min=0){
     return Math.floor((Math.random()*(max-min)) + min)
   }
+
+  swap (index1, index2){
+  	let storage = this[index1];
+  	this[index1] = this[index2];
+  	this[index2] = storage;
+  }
+
+  bubbleSort (){
+    for (var outer = this.length; outer >= 2; outer--){
+      for(var inner = 0; inner <= outer -1; inner ++){
+        if(this[inner] > this[inner+1]){
+          this.swap(inner,inner+1);
+        }
+      }
+    }
+  }
+
 }
 
 
@@ -23,6 +40,9 @@ class testArray extends Array {
 console.log ("---------------- SORTING ----------------")
 let randomNumArr = new testArray(10);
 randomNumArr.genData();
-console.log(randomNumArr)
-
+console.log(randomNumArr);
+randomNumArr.swap(0,1);
+console.log(randomNumArr);
+randomNumArr.bubbleSort();
+console.log(randomNumArr);
 
