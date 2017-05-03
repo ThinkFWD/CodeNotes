424f118ab3ffb05d5785a00ec9c1d7d985f1b61c
ThinkFWD <mike.thinkfwd@gmail.com>
2017-05-03 09:26:34 -0400

    starting sorting - generate random array to be sorted

---
+++ algorithms/sorting.js
@@ -0,0 +1,28 @@
+// +++++++++++++++++++++++++++++++++++++
+// ++++ Implementation of Graph ES6 ++++
+// +++++++++++++++++++++++++++++++++++++
+
+class testArray extends Array {
+  constructor(number){
+  	super(number);
+  	}
+
+  genData (){
+  	for(let x = 0; x < this.length; x++){
+  	  this[x] = this.randomNum()
+  	}
+  }
+
+  randomNum(max=100,min=0){
+    return Math.floor((Math.random()*(max-min)) + min)
+  }
+}
+
+
+
+console.log ("---------------- SORTING ----------------")
+let randomNumArr = new testArray(10);
+randomNumArr.genData();
+console.log(randomNumArr)
+
+
